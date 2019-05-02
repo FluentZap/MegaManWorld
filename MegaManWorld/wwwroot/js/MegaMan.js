d3a615d338cc81d296221c3628c63ff65cf3941c
Todd Aden <FluentZap@Gmail.com>
2019-05-01 20:26:13 -0700

    Add sprite control for PIXI

--- MegaManWorld/wwwroot/js/MegaMan.js
+++ MegaManWorld/wwwroot/js/MegaMan.js
@@ -14,40 +14,77 @@ $(function () {
   }, 100);
 });
 
+
 $(function () {
-const app = new PIXI.Application({
-    width: 300, height: 300, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
-});
-document.body.appendChild(app.view);
 
-const container = new PIXI.Container();
+	PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+	var Container = PIXI.Container,
+		autoDetectRenderer = PIXI.autoDetectRenderer,
+		loader = PIXI.loader,
+		resources = PIXI.loader.resources,
+		Sprite = PIXI.Sprite,
+		maxFrame = 2,
+		af = 0,
+		df = 100; // images per seconds
 
-app.stage.addChild(container);
+	//Create the renderer
+	var renderer = autoDetectRenderer(256, 256, { transparent: true});
+	
+	//Add the canvas to the HTML document
+	document.body.appendChild(renderer.view);
 
-// Create a new texture
-const texture = PIXI.Texture.from('../img/Chill.png');
+	//Create a container object called the `stage`
+	var stage = new Container();
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 1; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
-}
+	//Tell the `renderer` to `render` the `stage`
+	renderer.render(stage);
 
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+	renderer.view.style.position = "absolute";
+	renderer.view.style.display = "block";
+	renderer.autoResize = true;
+	renderer.resize(window.innerWidth, window.innerHeight);
+	
+	//renderer.resize(256, 256);
+	loader.add("megaman", "../img/chill12x3.png").load(setup);
+	
+	var megaman;
+	var run = [];
+	var megamanTexture;
 
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+	function setup() {
+		megamanTexture = loader.resources["megaman"].texture;
+		run.push(new PIXI.Rectangle(0, 0, 82, 82),
+			new PIXI.Rectangle(82 * 1, 0, 82, 82));			
+		megamanTexture.frame = run[0];
+		megaman = new Sprite(megamanTexture);
+		megaman.x = 0;
+		megaman.y = 0;
+		megaman.scale.x = 3.0;
+		megaman.scale.y = 3.0;
+		stage.addChild(megaman);
+		renderer.render(stage);
+	}
 
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
+	function gameLoop() {
+		var f = requestAnimationFrame(gameLoop);
+		if (af >= maxFrame) {
+			af = 0;
+		} else {
+			af += 1 / df;
+		}
+		megamanTexture.frame = run[Math.floor(af)];
+		stage.removeChild(megaman);
+		megaman = new Sprite(megamanTexture);
+		megaman.scale.x = 3.0;
+		megaman.scale.y = 3.0;
+		megaman.x = 10;
+		megaman.y = 10;
+		stage.addChild(megaman);
+		renderer.render(stage);
+	}
+
+
+
+	//Start the game loop
+	gameLoop();
 });
